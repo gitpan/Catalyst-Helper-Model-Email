4de6f71d8842097a86096ef3b1ae3361233ec953
Octavian Râşniţă <teddy@cpan.org>
2011-01-23 13:56:55 +0000

    Import of TEDDY/Catalyst-Helper-Model-Email-0.04 from CPAN.
    gitpan-cpan-distribution: Catalyst-Helper-Model-Email
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         TEDDY/Catalyst-Helper-Model-Email-0.04.tar.gz
    gitpan-cpan-author:       TEDDY
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Helper/Model/Email.pm
+++ lib/Catalyst/Helper/Model/Email.pm
@@ -2,26 +2,33 @@ package Catalyst::Helper::Model::Email;
 use strict;
 use warnings;
 
-our $VERSION = '0.03';
+use 5.008_008;
+
+our $VERSION = '0.04';
 
 sub mk_compclass {
-  my ($class, $helper, @mailer_args) = @_;
-  my %args;
-  @args{qw/mailer host username password/} = @mailer_args;
-  $helper->render_file('compclass', $helper->{file}, \%args);
+    my ( $class, $helper, @mailer_args ) = @_;
+    my %args;
+    @args{qw/mailer host username password/} = @mailer_args;
+    $helper->render_file( 'compclass', $helper->{file}, \%args );
+    return;
 }
 
 =head1 NAME
 
 Catalyst::Helper::Model::Email - Helper for Mail::Builder::Simple
 
-=head1 SYNOPSIS
+=head1 VERSION
 
-Running:
+Version 0.03
+
+=head1 SYNOPSIS
 
  ./script/myapp_create.pl model Email1 Email SMTP smtp.host.com usr passwd
 
-Will create C<MyApp::Model::Email1> that looks like:
+=head1 DESCRIPTION
+
+Using the command line above, Catalyst::Helper::Model::Email will create C<MyApp::Model::Email1> that looks like:
 
  package MyApp::Model::Email1;
  use strict;
@@ -70,7 +77,11 @@ But you will be also able to send more complex email messages like:
 
 ...or even more complex messages, using templates.
 
-=head1 ARGUMENTS
+=head1 SUBROUTINES/METHODS
+
+=head2 mk_compclass
+
+=head1 CONFIGURATION AND ENVIRONMENT
 
  ./script/myapp_create.pl model <model_name> Email <mailer_args>
 
@@ -82,7 +93,7 @@ If you want to use an SMTP server, you need to add just SMTP and the address of 
 
 If you want to use an SMTP server that requires authentication, you need to add SMTP, the address of the server, the username and the password, like in the exemple given above.
 
-The module supports the mailers supported by L<Mail::Builder::Simple>. Mail::Builder::Simple uses L<Email::Sender> for sending email, so check the modules under L<Email::Sender::Transport> for finding the parameters you might need to use for each type of mailer.
+The module supports the mailers supported by L<Mail::Builder::Simple|Mail::Builder::Simple>. Mail::Builder::Simple uses L<Email::Sender|Email::Sender> for sending email, so check the modules under L<Email::Sender::Transport|Email::Sender::Transport> for finding the parameters you might need to use for each type of mailer.
 
 This helper can add in the model just the mailer type, the hostname, the username and the password, but you can add manually other parameters like a different port than the default, or the option for using SSL when connecting to the SMTP server.
 
@@ -105,15 +116,25 @@ You can also put the configuration variables in the application's main configura
    </args>
  </Model::Email1>
 
-=head1 SEE ALSO
+=head1 DIAGNOSTICS
 
-L<Catalyst>, L<Mail::Builder::Simple>, L<Email::Sender>, L<Mail::Builder>
+=head1 DEPENDENCIES
+
+L<Catalyst|Catalyst>, L<Mail::Builder::Simple|Mail::Builder::Simple>, L<Email::Sender|Email::Sender>, L<Mail::Builder|Mail::Builder>
+
+=head1 INCOMPATIBILITIES
+
+No known incompatibilities.
+
+=head1 BUGS AND LIMITATIONS
+
+No known bugs. If you find some, please announce.
 
 =head1 AUTHOR
 
 Octavian Rasnita C<orasnita@gmail.com>
 
-=head1 LICENSE
+=head1 LICENSE AND COPYRIGHT
 
 This library is free software. You can redistribute it and/or modify
 it under the same terms as perl itself.
